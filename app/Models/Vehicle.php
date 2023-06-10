38059696c7658d1c275f752ff28a8609952f3492
Vadim Khorobrykh <bubaololo@gmail.com>
2023-06-11 05:55:21 +0600

    vehikles and zones created

--- app/Models/Vehicle.php
+++ app/Models/Vehicle.php
@@ -2,8 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 
 class Vehicle extends Model
 {
@@ -11,4 +14,12 @@ class Vehicle extends Model
     use SoftDeletes;
  
     protected $fillable = ['user_id', 'plate_number'];
+    
+    protected static function booted()
+    {
+        static::addGlobalScope('user', function (Builder $builder) {
+            $builder->where('user_id', auth()->id());
+        });
+    }
+    
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\V1\VehicleController;
+use App\Http\Controllers\Api\V1\ZoneController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\Api\V1\Auth;
@@ -18,13 +20,19 @@ use \App\Http\Controllers\Api\V1\Auth;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::post('auth/register', Auth\RegisterController::class);
 
-Route::post('auth/login', Auth\LoginController::class);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('profile', [Auth\ProfileController::class, 'show']);
     Route::put('profile', [Auth\ProfileController::class, 'update']);
     Route::put('password', Auth\PasswordUpdateController::class);
     Route::post('auth/logout', Auth\LogoutController::class);
+    
+    Route::apiResource('vehicles', VehicleController::class);
 });
+
+Route::post('auth/register', Auth\RegisterController::class);
+
+Route::post('auth/login', Auth\LoginController::class);
+
+Route::get('zones', [ZoneController::class, 'index']);
