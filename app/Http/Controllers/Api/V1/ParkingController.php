b1bcc9101afeee986a4329159990bf21053df105
Vadim Khorobrykh <bubaololo@gmail.com>
2023-06-11 20:31:39 +0600

    controllers ready

--- app/Http/Controllers/Api/V1/ParkingController.php
+++ app/Http/Controllers/Api/V1/ParkingController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Services\ParkingPriceService;
 use Illuminate\Http\Request;
 use App\Http\Resources\ParkingResource;
 use App\Models\Parking;
@@ -42,6 +43,7 @@ class ParkingController extends Controller
     {
         $parking->update([
             'stop_time' => now(),
+            'total_price' => ParkingPriceService::calculatePrice($parking->zone_id, $parking->start_time),
         ]);
         
         return ParkingResource::make($parking);
